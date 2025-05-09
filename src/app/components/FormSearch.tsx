94edcca0a24b4ed49faed563cf643dd1c0db9def
Carlos M <53409854+Cema2019@users.noreply.github.com>
2025-05-09 13:26:34 -0600

    Replace form component with a div

--- src/app/components/FormSearch.tsx
+++ src/app/components/FormSearch.tsx
@@ -37,23 +37,18 @@ const debouncedUsername = useDebounce(username, 500);
 
 
     return (
-        <form 
-        onSubmit={handleSubmit}
-        className="mb-6 flex flex-wrap items-center gap-2 bg-blue-200 dark:bg-blue-900 p-4 rounded-xl">
-            <span className="min-w-[20px]">
-                <SearchIcon className="fill-sky-500 dark:fill-sky-400" />
-            </span>
-            <input
-                className="flex-1 h-14 p-2 rounded-lg bg-transparent text-slate-900 dark:text-white placeholder:text-slate-600 dark:placeholder:text-white focus:ring-2 focus:ring-sky-500"
-                type="text"
-                placeholder="Search github username..."
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            ></input>
-            <button className="bg-sky-500 dark:bg-sky-600 rounded-lg py-4 px-4 text-white font-bold">
-                Search
-            </button>
-        </form>
+        <div className="mb-6 flex flex-wrap items-center gap-2 bg-blue-200 dark:bg-blue-900 p-4 rounded-xl">
+      <span className="min-w-[20px]">
+        <SearchIcon className="fill-sky-500 dark:fill-sky-400" />
+      </span>
+      <input
+        className="flex-1 h-14 p-2 rounded-lg bg-transparent text-slate-900 dark:text-white placeholder:text-slate-600 dark:placeholder:text-white focus:ring-2 focus:ring-sky-500"
+        type="text"
+        placeholder="Search github username..."
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+    </div>
     );
 };
 export default FormSearch;
